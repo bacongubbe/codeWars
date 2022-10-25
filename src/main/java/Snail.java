187b71d1fd3a19b7e579a210b4acb9d34a7d4e03
Felix <berg.felix@hotmail.com>
2022-10-25 16:21:50 +0200

    Snail fixed

--- src/main/java/Snail.java
+++ src/main/java/Snail.java
@@ -12,18 +12,46 @@ public class Snail {
 
         if (array[0].length == 0) return new int[]{0};
 
-        int[] snail = new int[array.length * array.length];
+        int[] snail = new int[array[0].length * array[0].length];
 
         int y = 0;
         int x = 0;
         int spiral = 0;
-        int i = 0;
+        int direction = 0;
+        int i = -1;
 
-        while (i < snail.length){
+        while (i < snail.length -1){
+        i++;
+        snail[i] = array[y][x];
 
-
+        if (direction == 0){
+            x++;
+            if (x == array[0].length - spiral -1) {
+                direction = 1;
+                continue;
+            }
         }
-
+        if (direction == 1){
+            y++;
+            if (y == array[0].length - spiral -1) {
+                direction = 2;
+                continue;
+            }
+        }
+        if (direction == 2) {
+            x--;
+            if (x == spiral) {
+                direction = 3;
+                spiral++;
+                continue;
+            }
+        }
+        if (direction == 3){
+            y--;
+            if (y == spiral) {
+                direction = 0;
+            }
+        }}
 
         return snail;
     }
